5823ee9b44bc8b97123f7b2e5fb214eb789c63c7
Praveen Kumar Gogula <praveengogula20@gmail.com>
2015-07-01 14:09:55 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 var config = {}
 
-config.host = process.env.HOST || "https://documentdbdatabase.documents.azure.com:443/";
-config.authKey = process.env.AUTH_KEY || "GrpCX546l828F5deJV1OPlyvpEbch5idEsAes8l96Hg9tqSeTj3xl4ZnoQwRcTqC6p8j2L7Ej64cLxSuSjDBWQ==";
+config.host = process.env.HOST || "https://praveendocumentdb1.documents.azure.com:443/";
+config.authKey = process.env.AUTH_KEY || "pSEZLvHToW1LiIVPaoiwPPbYns7sWlYQC1bwttH50UgwVVDzjDMiAfzeke7itFwIGtT2kMJlKuzRw1cjRiz4tw==";
 config.databaseId = "ToDoList";
 config.collectionId = "Items";
 
